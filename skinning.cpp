c4e8d5b995bdeec92fe663ba90932da6abc99fe8
Nitzan Saar <nitzansaar1@gmail.com>
2026-04-18 17:15:05 -0700

    implement Linear Blend Skinning in applySkinning method.

--- skinning.cpp
+++ skinning.cpp
@@ -9,20 +9,26 @@ using namespace std;
 // CSCI 520 Computer Animation and Simulation
 // Jernej Barbic and Yijing Li
 
+
+/*
+ * Constructor for the Skinning class.
+ * Loads the skinning weights from a file and stores them in the meshSkinningJoints and meshSkinningWeights arrays.
+ * Also computes the number of joints influencing each vertex.
+ */
 Skinning::Skinning(int numMeshVertices, const double * restMeshVertexPositions,
     const std::string & meshSkinningWeightsFilename)
 {
-  this->numMeshVertices = numMeshVertices;
-  this->restMeshVertexPositions = restMeshVertexPositions;
+  this->numMeshVertices = numMeshVertices; // store the number of vertices in the mesh
+  this->restMeshVertexPositions = restMeshVertexPositions; // store the rest vertex positions
 
   cout << "Loading skinning weights..." << endl;
-  ifstream fin(meshSkinningWeightsFilename.c_str());
+  ifstream fin(meshSkinningWeightsFilename.c_str()); // open the file containing the skinning weights
   assert(fin);
   int numWeightMatrixRows = 0, numWeightMatrixCols = 0;
-  fin >> numWeightMatrixRows >> numWeightMatrixCols;
+  fin >> numWeightMatrixRows >> numWeightMatrixCols; // read the number of rows and columns from the file
   assert(fin.fail() == false);
-  assert(numWeightMatrixRows == numMeshVertices);
-  int numJoints = numWeightMatrixCols;
+  assert(numWeightMatrixRows == numMeshVertices); // check that the number of rows is equal to the number of vertices in the mesh
+  int numJoints = numWeightMatrixCols; // store the number of joints
 
   vector<vector<int>> weightMatrixColumnIndices(numWeightMatrixRows);
   vector<vector<double>> weightMatrixEntries(numWeightMatrixRows);
@@ -41,6 +47,7 @@ Skinning::Skinning(int numMeshVertices, const double * restMeshVertexPositions,
 
   // Build skinning joints and weights.
   numJointsInfluencingEachVertex = 0;
+  // find the maximum number of joints influencing each vertex
   for (int i = 0; i < numMeshVertices; i++)
     numJointsInfluencingEachVertex = std::max(numJointsInfluencingEachVertex, (int)weightMatrixEntries[i].size());
   assert(numJointsInfluencingEachVertex >= 2);
@@ -73,14 +80,29 @@ Skinning::Skinning(int numMeshVertices, const double * restMeshVertexPositions,
 
 void Skinning::applySkinning(const RigidTransform4d * jointSkinTransforms, double * newMeshVertexPositions) const
 {
-  // Students should implement this
-
-  // The following below is just a dummy implementation.
-  for(int i=0; i<numMeshVertices; i++)
+  // Linear Blend Skinning:
+  //   p' = sum_i ( w_i * M_i * p_rest )
+  // where M_i = jointSkinTransforms[jointIndex_i] = globalTransform * globalRestTransform^{-1}
+  // and the weights w_i sum to 1 for each vertex.
+  for (int v = 0; v < numMeshVertices; v++)
   {
-    newMeshVertexPositions[3 * i + 0] = restMeshVertexPositions[3 * i + 0];
-    newMeshVertexPositions[3 * i + 1] = restMeshVertexPositions[3 * i + 1];
-    newMeshVertexPositions[3 * i + 2] = restMeshVertexPositions[3 * i + 2];
+    Vec3d pRest(restMeshVertexPositions[3 * v + 0],
+                restMeshVertexPositions[3 * v + 1],
+                restMeshVertexPositions[3 * v + 2]);
+
+    Vec3d pNew(0.0, 0.0, 0.0);
+    for (int k = 0; k < numJointsInfluencingEachVertex; k++)
+    {
+      int idx = v * numJointsInfluencingEachVertex + k;
+      double w = meshSkinningWeights[idx];
+      if (w == 0.0) continue; // skip unused/zero-weight slots
+      int jointID = meshSkinningJoints[idx];
+      pNew += w * jointSkinTransforms[jointID].transformPoint(pRest);
+    }
+
+    newMeshVertexPositions[3 * v + 0] = pNew[0];
+    newMeshVertexPositions[3 * v + 1] = pNew[1];
+    newMeshVertexPositions[3 * v + 2] = pNew[2];
   }
 }
 
